f8f8903a5e664f07dc11f22eb883cb1a5e58558c
Creeper Lv <creeperlv38@outlook.com>
2021-05-12 16:10:29 +0800

    Fixed an logic error in the Motion Radar. Simplified the logic of Motion Radar.

--- Assets/HUDNavi/RadarCore.cs
+++ Assets/HUDNavi/RadarCore.cs
@@ -38,6 +38,7 @@ namespace HUDNavi
         {
             if (isOn == true)
             {
+                var rotatoin = (ReverseHorizontalRotation ? -1 : 1);
                 foreach (var item in Targets)
                 {
                     if (item.isActiveAndEnabled == false)
@@ -48,66 +49,40 @@ namespace HUDNavi
                     float _x = (item.transform.position.x - Detector.transform.position.x) / Distance;
                     float _y = (item.transform.position.z - Detector.transform.position.z) / Distance;
                     bool isMoving = item.isMoving;
-                    if (_x * _x + _y * _y > 1)
+                    if (item.isAlwaysDetected == true) isMoving = true;
+                    if (isMoving == true)
                     {
-                        // overflow boundary
-                        if (item.ShowBeyoundBoundary == false)
+                        if (_x * _x + _y * _y > 1)
                         {
-                            if (item.RadarPoint.activeSelf == true) item.RadarPoint.SetActive(false);
-                        }
-                        else
-                        {
-                            if (isMoving == true)
-                            {
-                                if (item.RadarPoint.activeSelf == false) item.RadarPoint.SetActive(true);
-
-                            }
-                            else
-                            {
-                                if (item.isAlwaysDetected == true)
-                                {
-
-                                    if (item.RadarPoint.activeSelf == false) item.RadarPoint.SetActive(true);
-                                }
-                                else
-                                {
-                                    if (item.RadarPoint.activeSelf == true) item.RadarPoint.SetActive(false);
-                                }
-                            }
-                            if (item.RadarPoint.activeSelf == false) item.RadarPoint.SetActive(true);
-                            var _len = Mathf.Sqrt(_x * _x + _y * _y);
-                            var ratio = 1 / _len;
-                            var rt = item.RadarPoint.transform as RectTransform;
-                            rt.anchoredPosition = new Vector2(_x * RadarRadius * ratio, _y * RadarRadius * ratio);
-                        }
-
-                    }
-                    else
-                    {
-                        if (isMoving == true)
-                        {
-                            if (item.RadarPoint.activeSelf == false) item.RadarPoint.SetActive(true);
-
-                        }
-                        else
-                        {
-                            if (item.isAlwaysDetected == true)
-                            {
-
-                                if (item.RadarPoint.activeSelf == false) item.RadarPoint.SetActive(true);
-                            }
-                            else
+                            if (item.ShowBeyoundBoundary == false)
                             {
                                 if (item.RadarPoint.activeSelf == true) item.RadarPoint.SetActive(false);
                             }
+                            else
+                            {
+                                if (item.RadarPoint.activeSelf == false) item.RadarPoint.SetActive(true);
+
+                                var _len = Mathf.Sqrt(_x * _x + _y * _y);
+                                var ratio = 1 / _len;
+                                var rt = item.RadarPoint.transform as RectTransform;
+                                rt.anchoredPosition = new Vector2(_x * RadarRadius * ratio, _y * RadarRadius * ratio);
+                            }
                         }
-                        var rt = item.RadarPoint.transform as RectTransform;
-                        rt.anchoredPosition = new Vector2(_x * RadarRadius, _y * RadarRadius);
-                        //rt.anchoredPosition=
+                        else
+                        {
+                            if (item.RadarPoint.activeSelf == false) item.RadarPoint.SetActive(true);
+                            var rt = item.RadarPoint.transform as RectTransform;
+                            rt.anchoredPosition = new Vector2(_x * RadarRadius, _y * RadarRadius);
+                        }
                     }
+                    else
+                    {
+                        if (item.RadarPoint.activeSelf == true) item.RadarPoint.SetActive(false);
+                    }
+
                 }
                 {
-                    RadarObject.transform.localEulerAngles = new Vector3(0, 0, North_Base_Angle + ((ReverseHorizontalRotation ? 1 : -1) * Detector.transform.localEulerAngles.y));
+                    RadarObject.transform.localEulerAngles = new Vector3(0, 0, North_Base_Angle + (rotatoin * Detector.transform.localEulerAngles.y));
                 }
             }
             else
